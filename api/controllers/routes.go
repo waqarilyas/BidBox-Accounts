74870096d4b9f8741ac9ffad7137ba9a587a5e2f
shaheer <shaheer.ahmad@kryptomind.com>
2023-06-16 16:00:58 +0500

    Fix: removed Auth

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -44,7 +44,7 @@ func (r *Server) initializeRoutes() {
 	s.HandleFunc("/otp/generate", middleware.MiddlewareJSON(r.GenerateOTP)).Methods("POST")
 	s.HandleFunc("/otp/verify", middleware.MiddlewareJSON(r.VerifyOTP)).Methods("POST")
 	s.HandleFunc("/otp/validate", middleware.MiddlewareJSON(r.ValidateOTP)).Methods("POST")
-	s.HandleFunc("/changePassword", middleware.MiddlewareAuth(r.ChangePassword)).Methods("POST")
+	s.HandleFunc("/changePassword", middleware.MiddlewareJSON(r.ChangePassword)).Methods("POST")
 
 	s.HandleFunc("/history", middleware.ValidateEmail(r.GetPositionHistory)).Methods("GET")
 
