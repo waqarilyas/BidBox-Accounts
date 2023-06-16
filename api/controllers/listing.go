3cb9d0672654eb8c0990ccc7540f2dc0e1d7af65
ahmed-023 <ahmed.nadeem@kryptomind.com>
2023-06-16 12:31:04 +0500

    feat: pagination statements

--- api/controllers/listing.go
+++ api/controllers/listing.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/kryptomind/bidboxapi/AccountsService/api/models"
 	"github.com/kryptomind/bidboxapi/AccountsService/api/response"
@@ -11,21 +12,35 @@ import (
 func (server *Server) ListToday(w http.ResponseWriter, r *http.Request, email string) {
 	service := r.URL.Query().Get("service")
 
+	// Parse query parameters
+	page, err := strconv.Atoi(r.URL.Query().Get("page"))
+	if err != nil {
+		page = 1
+	}
+	limit, err := strconv.Atoi(r.URL.Query().Get("limit"))
+	if err != nil {
+		limit = 10
+	}
+
+	// Calculate offset based on page and limit
+	offset := (page - 1) * limit
+
 	if service == "" {
 		response.ERROR(w, http.StatusBadRequest, errors.New("service is required"))
 		return
 	}
 
 	st := models.Statements{}
-	sts, err := st.GetStatementsToday(server.DB, email, service)
+	sts, err := st.GetStatementsToday(server.DB, email, service, limit, offset)
 	if err != nil {
 		response.ERROR(w, http.StatusInternalServerError, err)
 		return
 	}
 
 	if len(*sts) == 0 {
-		res := make(map[string]string)
+		res := make(map[string]interface{})
 		res["msg"] = "no statements today"
+		res["data"] = []string{}
 		response.JSON(w, http.StatusOK, res)
 		return
 	}
@@ -39,17 +54,30 @@ func (server *Server) ListTotal(w http.ResponseWriter, r *http.Request, email st
 		response.ERROR(w, http.StatusBadRequest, errors.New("service is required"))
 		return
 	}
+	// Parse query parameters
+	page, err := strconv.Atoi(r.URL.Query().Get("page"))
+	if err != nil {
+		page = 1
+	}
+	limit, err := strconv.Atoi(r.URL.Query().Get("limit"))
+	if err != nil {
+		limit = 10
+	}
+
+	// Calculate offset based on page and limit
+	offset := (page - 1) * limit
 
 	st := models.Statements{}
-	sts, err := st.GetStatementsAllTime(server.DB, email, service)
+	sts, err := st.GetStatementsAllTime(server.DB, email, service, limit, offset)
 	if err != nil {
 		response.ERROR(w, http.StatusInternalServerError, err)
 		return
 	}
 
 	if len(*sts) == 0 {
-		res := make(map[string]string)
+		res := make(map[string]interface{})
 		res["msg"] = "no statements for this user"
+		res["data"] = []string{}
 		response.JSON(w, http.StatusOK, res)
 		return
 	}
