5b446ef2fa02fa4e00560a3da8921f30571458e5
ahmed-023 <ahmed.nadeem@kryptomind.com>
2023-06-19 15:03:51 +0500

    feat: coin update

--- api/models/admin/Coinpairs.go
+++ api/models/admin/Coinpairs.go
@@ -3,8 +3,19 @@ package admin
 import "github.com/jinzhu/gorm"
 
 type CoinPair struct {
-	Id   int    `json:"id"`
-	Coin string `json:"coin"`
+	Id       int    `json:"id"`
+	Coin     string `json:"coin"`
+	Active   bool
+	Leverage int
+}
+
+func (c *CoinPair) Validate(prev *CoinPair) {
+	if c.Leverage == 0 {
+		c.Leverage = prev.Leverage
+	}
+	if c.Active == prev.Active {
+		c.Active = prev.Active
+	}
 }
 
 func (cp *CoinPair) GetAllCoins(db *gorm.DB) (*[]CoinPair, error) {
@@ -12,6 +23,15 @@ func (cp *CoinPair) GetAllCoins(db *gorm.DB) (*[]CoinPair, error) {
 	err := db.Debug().Model(&CoinPair{}).Limit(100).Find(&Coins).Error
 	if err != nil {
 		return &[]CoinPair{}, err
+	}
+	return &Coins, nil
+}
+
+func (cp *CoinPair) GetCoinById(db *gorm.DB, coin string) (*CoinPair, error) {
+	Coins := CoinPair{}
+	err := db.Debug().Model(&CoinPair{}).Where("coin = ?", coin).Take(&Coins).Error
+	if err != nil {
+		return &CoinPair{}, err
 	}
 	return &Coins, nil
 }
@@ -27,7 +47,9 @@ func (cp *CoinPair) SaveCoinPair(db *gorm.DB) (*CoinPair, error) {
 func (cp *CoinPair) UpdateCoinPair(db *gorm.DB, coin string) (*CoinPair, error) {
 	db = db.Debug().Model(&CoinPair{}).Where("coin = ?", coin).Take(&CoinPair{}).UpdateColumns(
 		map[string]interface{}{
-			"coin": cp.Coin,
+			"coin":     cp.Coin,
+			"active":   cp.Active,
+			"Leverage": cp.Leverage,
 		},
 	)
 	if db.Error != nil {
