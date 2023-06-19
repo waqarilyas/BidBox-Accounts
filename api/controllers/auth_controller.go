2eb9faeea20a2f6dbfa17f534bbf779814ea3a3f
ahmed-023 <ahmed.nadeem@kryptomind.com>
2023-06-19 12:47:45 +0500

    fix: cors issue

--- api/controllers/auth_controller.go
+++ api/controllers/auth_controller.go
@@ -126,12 +126,7 @@ func (s *Server) GenerateOTP(w http.ResponseWriter, r *http.Request) {
 	response.JSON(w, http.StatusOK, otpResponse)
 }
 
-func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-}
-
 func (s *Server) VerifyOTP(w http.ResponseWriter, r *http.Request) {
-	enableCors(&w)
 	var payload *admin.OTPInput
 
 	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
@@ -368,7 +363,6 @@ func (server *Server) GetTimeframe(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	
 	res := make(map[string]interface{})
 	res["timeframe"] = timeframe
 	response.JSON(w, http.StatusOK, res)
